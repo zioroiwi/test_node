983b154936f17e6b0d8129466b313ff5f038107f
zioroiwi <khacdien.vn96@gmail.com>
2023-06-09 10:09:56 +0700

    video #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,17 @@
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    console.log("--------------------------------");
+    console.log(data);
+    console.log("--------------------------------");
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 let getAboutPage = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine.js";
 import initWebRoute from "./route/web.js";
 require("dotenv").config();
+import connectDB from "./config/connectDB.js";
 
 let app = express();
 
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoute(app);
+
+connectDB();
 
 let port = process.env.PORT || 3000;
 // nếu chưa khai báo port trong file .env thì sẽ sử dụng port 3000
