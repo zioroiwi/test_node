c731089707315bb8ef9deda866127fbc65cb072b
zioroiwi <khacdien.vn96@gmail.com>
2023-06-09 09:22:58 +0700

    video #20

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,11 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRouter = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello World!");
-  });
+  router.get("/", homeController.getHomePage);
+  router.get("/about", homeController.getAboutPage);
   return app.use("/", router);
 };
 
