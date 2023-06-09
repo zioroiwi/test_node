f201c6413e815058970ca69ec28de7bd23f85ad8
zioroiwi <khacdien.vn96@gmail.com>
2023-06-09 21:05:31 +0700

    video #25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,6 @@
 import db from "../models/index";
+import CRUDService from "../servers/CRUDService";
+import createNewUser from "../servers/CRUDService";
 
 let getHomePage = async (req, res) => {
   try {
@@ -18,8 +20,20 @@ let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
 
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let messenge = await CRUDService.createNewUser(req.body);
+  console.log(messenge);
+  return res.send("post crud from server");
+};
+
 // objecctK{key,value}
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
---
+++ src/servers/CRUDService.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import db from "../models/index";
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashUserPasswordBrcypt = await hashUserPassword(data.password);
+      await db.User.create({
+        email: data.email,
+        password: hashUserPasswordBrcypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        phonenumber: data.phonenumber,
+        gender: data.gender === "1" ? "true" : "false",
+        roleId: data.roleId,
+      });
+      resolve("success");
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      var hashUserPassword = await bcrypt.hashSync(password, salt);
+      resolve(hashUserPassword);
+    } catch (error) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  createNewUser: createNewUser,
+};
