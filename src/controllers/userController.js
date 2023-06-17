ebdfe81b7f0a0d51a67b20c50c2cf885b1b8f998
zioroiwi <khacdien.vn96@gmail.com>
2023-06-17 21:55:58 +0700

    video #37

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,10 +15,29 @@ let handleLogin = async (req, res) => {
   return res.status(200).json({
     errCode: userData.errCode,
     message: userData.errMsg,
-    user: userData.user ? userData.user : {}
+    user: userData.user ? userData.user : {},
+  });
+};
+
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id; //ALL, id
+
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMsg: "Missing id",
+      users: [],
+    });
+  }
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMsg: "OK",
+    users,
   });
 };
 
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
