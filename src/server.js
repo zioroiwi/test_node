3fed05e9b105fd6fbbee26c9ce73397228131b7a
zioroiwi <khacdien.vn96@gmail.com>
2023-06-17 20:11:53 +0700

    video #36

--- src/server.js
+++ src/server.js
@@ -4,10 +4,37 @@ import viewEngine from "./config/viewEngine.js";
 import initWebRoute from "./route/web.js";
 require("dotenv").config();
 import connectDB from "./config/connectDB.js";
+import cors from "cors";
 
 let app = express();
 
 // config app
+// app.use(cors({ origin: true }));
+// middleware cors
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
